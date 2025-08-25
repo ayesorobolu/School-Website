e6158225f77c5fcaaad75c1e83608d617675a409
ayesorobolu <abrahamayesoro@gmail.com>
2025-08-25 18:40:56 +0100

    feat: add hero section, new assets, and fix styling
    - Added hero component with heading
    - Included new image and icon assets
    - Fixed navbar styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Navbar from './component/Navbar.jsx'
+import Homepage from './pages/Homepage.jsx'
 
 function App() {
   return (
     <>
 
     <Navbar/>
-   
+    <Homepage/>
     </>
   )
 }
---
+++ src/component/Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='hero'>
+      <h1 className='hero-text'>Welcome to <br /> Crystal Model School</h1>
+    </div>
+  )
+}
+
+export default Hero
